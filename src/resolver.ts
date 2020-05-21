34e3aa83d4017fbd6fe2c0980919df232f0254d4
csr632 <632882184@qq.com>
2020-05-21 22:14:24 +0800

    fix: make sure react is externaled (#5)

--- src/resolver.ts
+++ src/resolver.ts
@@ -3,10 +3,10 @@ import { Resolver } from 'vite'
 export const resolver: Resolver = {
   alias(id) {
     const isProd = process.env.NODE_ENV === 'production'
-    if (id === 'react') {
+    if (id === 'react' || id === '@pika/react') {
       return isProd ? '@pika/react' : '@pika/react/source.development.js'
     }
-    if (id === 'react-dom') {
+    if (id === 'react-dom' || id === '@pika/react-dom') {
       return isProd
         ? '@pika/react-dom'
         : '@pika/react-dom/source.development.js'
