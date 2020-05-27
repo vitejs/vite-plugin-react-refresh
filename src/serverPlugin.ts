ec05b15c8282f85b8675252d86d56270afbf49e9
csr632 <632882184@qq.com>
2020-05-27 14:21:13 +0800

    fix: add globalPreamble check to discover edge cases

--- src/serverPlugin.ts
+++ src/serverPlugin.ts
@@ -9,6 +9,7 @@ import RefreshRuntime from "${runtimePublicPath}"
 RefreshRuntime.injectIntoGlobalHook(window)
 window.$RefreshReg$ = () => {}
 window.$RefreshSig$ = () => (type) => type
+window.__vite_plugin_react_preamble_installed__ = true
 </script>
 `
 
