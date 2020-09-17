0164c8ce1b839555668203529b2795b6117221e4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-09-17 16:46:48 -0400

    fix: resolve with absolute path
    This ensures the same "@pika/react" package is used by all modules,
    even modules that exist outside the project root (ie: when a package
    is linked using "yarn link" or similar).

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,15 +1,17 @@
 import { Resolver } from 'vite'
+import path from 'path'
 
 export const resolver: Resolver = {
   alias(id) {
-    const isProd = process.env.NODE_ENV === 'production'
-    if (id === 'react' || id === '@pika/react') {
-      return isProd ? '@pika/react' : '@pika/react/source.development.js'
-    }
-    if (id === 'react-dom' || id === '@pika/react-dom') {
-      return isProd
-        ? '@pika/react-dom'
-        : '@pika/react-dom/source.development.js'
+    if (/^(@pika\/)?react(-dom)?$/.test(id)) {
+      if (process.env.NODE_ENV !== 'production') {
+        id += '/source.development.js'
+      }
+      return path.join(
+        process.cwd(),
+        'node_modules',
+        id.replace(/^(@pika\/)?/, '@pika' + path.sep)
+      )
     }
   }
 }
