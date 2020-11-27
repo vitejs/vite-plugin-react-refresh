701b46971e61e095962b6ac5902b0cfa1b527353
Sirui Chen <632882184@qq.com>
2020-11-28 00:06:21 +0800

    feat: use official react
    BREAKING CHANGE: It require vite>=1.0.0-rc.13. And user should install official react.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { resolver } from './resolver'
 import { reactRefreshServerPlugin } from './serverPlugin'
 import { reactRefreshTransform } from './transform'
 
 module.exports = {
-  resolvers: [resolver],
   configureServer: reactRefreshServerPlugin,
   transforms: [reactRefreshTransform]
 }
