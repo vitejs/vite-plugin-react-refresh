e2ba377a815023efbc511300066ca184bec23efc
Evan You <yyx990803@gmail.com>
2020-12-03 14:40:09 -0500

    refactor: change plugin export for plugin typing
    close #7

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { Plugin } from 'vite'
 import { reactRefreshServerPlugin } from './serverPlugin'
 import { reactRefreshTransform } from './transform'
 
-module.exports = {
+const plugin: Plugin = {
   configureServer: reactRefreshServerPlugin,
   transforms: [reactRefreshTransform]
 }
+
+export = plugin
